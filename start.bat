acf16c2777cfc06d5bba0e859c0dd3718a1879ac
Daniil Gatman <daniilhatman@gmail.com>
2024-07-23 11:44:59 +0300

    Edited start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,7 @@
 @echo off
-cd C:\Users\%USERNAME%\VirtAppRelease\
-git pull
-VirtApp.exe
+:loop
+
+tasklist /fi “ImageName eq VirtApp.exe” /fo csv 2>NUL | find /I “VLC.exe”>NUL
+
+if “%ERRORLEVEL%”==”0”  (echo Process / Application is running) else (cd C:\Users\%USERNAME%\VirtAppRelease\ && git pull && VirtApp.exe)
+goto loop
